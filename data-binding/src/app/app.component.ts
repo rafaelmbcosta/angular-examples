d5d5b007a82513a089ec277ac6663a1246f31773
Rafael Costa <rafaelmbcosta@gmail.com>
2017-04-01 14:59:51 -0300

    Example 02 - Data Binding

---
+++ data-binding/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  userName = "";
+  allowReset = false;
+
+  resetUserName(){
+    this.userName = "";
+  }
+
+  onUpdateUserName(event){
+    console.log(event);
+    if (event.target.value != ""){
+      this.allowReset = true;
+    } else {
+      this.allowReset = false;
+    }
+  }
+}
